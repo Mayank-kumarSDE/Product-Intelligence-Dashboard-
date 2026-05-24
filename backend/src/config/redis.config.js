2037f7390ae632bd2ba2e6e548ed7bfed027b1d2
Mayank Kumar <mayankkumar8481@gmail.com>
2026-05-25 05:29:06 +0530

    Fix: Upstash TLS support for BullMQ Redis connection

--- backend/src/config/redis.config.js
+++ backend/src/config/redis.config.js
@@ -1,6 +1,10 @@
 import IORedis from "ioredis";
 import { env } from "./env.config.js";
 
+const isTls = env.redisUrl.startsWith("rediss://");
+
 export const redisConnection = new IORedis(env.redisUrl, {
-  maxRetriesPerRequest: null
+  maxRetriesPerRequest: null,
+  enableReadyCheck: false,
+  tls: isTls ? {} : undefined
 });
